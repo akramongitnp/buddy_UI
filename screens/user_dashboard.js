ead95a04f561051db18e19a2d7f46e451e84e6c6
akramongitnp <akramsiddikinp@gmail.com>
2023-02-06 23:54:48 +0530

    src added

---
+++ screens/user_dashboard.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { StyleSheet,View, Text } from "react-native"
+
+export default function user_dashboard() {
+    return(
+        <View style={styles.container}>
+            <View style={styles.upper_area}>
+                <Text>Upper Area</Text>
+            </View>
+            <View style={styles.lower_area}>
+                <Text>Lower Area</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#FFF",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    upper_area: {
+        backgroundColor: "#CAFF33",
+        
+    },
+    lower_area: {
+        backgroundColor: "#FF336E",
+        padding: "15%"
+    }
+})
