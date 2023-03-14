78278bc49691abff665c1d9cac9498c5f6e700b5
akramongitnp <akramsiddikinp@gmail.com>
2023-03-14 20:42:37 +0530

    NA

--- screens/user_dashboard.js
+++ screens/user_dashboard.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { StyleSheet,View, Text } from "react-native"
+import { StyleSheet,View, Text, SafeAreaView } from "react-native"
 
 export default function user_dashboard() {
     return(
-        <View style={styles.container}>
-            <View style={styles.upper_area}>
-                <Text>Upper Area</Text>
+        <SafeAreaView>
+            <View style={styles.container}>
+                <View style={styles.upper_area}>
+                    <Text>Upper area</Text>
+                </View>
+                <View style={styles.lower_area}>
+                    <Text>Lower area</Text>
+                </View>
             </View>
-            <View style={styles.lower_area}>
-                <Text>Lower Area</Text>
-            </View>
-        </View>
+        </SafeAreaView>
     )
 }
 
@@ -23,8 +25,10 @@ const styles = StyleSheet.create({
     },
     upper_area: {
         backgroundColor: "#CAFF33",
+        flex: 2,
     },
     lower_area: {
         backgroundColor: "#FF336E",
+        flex: 4,
     }
 })
