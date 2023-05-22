246e8657d12973edf2a986fc7f2190f6f46b595c
akramongitnp <akramsiddikinp@gmail.com>
2023-05-22 21:36:49 +0530

    false

--- App.js
+++ App.js
@@ -10,5 +10,14 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName='Auth'>
+          <Stack.Screen name='Auth' component={Auth_Screen}/>
+          <Stack.Screen name='User_dashboard' component={User_dashboard}/>
+          <Stack.Screen name='Try' component={Try}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
